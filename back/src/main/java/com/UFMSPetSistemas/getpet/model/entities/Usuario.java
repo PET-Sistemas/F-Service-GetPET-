ba5f96e47703dbf0cd7189e9020f220b4ba2afe9
Cauê <cauemor@gmail.com>
2024-05-06 15:13:46 -0400

    :sparkles: feat(Usuario.java): Adicionando annotations a classe para adaptação ao banco de dados.

--- back/src/main/java/com/UFMSPetSistemas/getpet/model/entities/Usuario.java
+++ back/src/main/java/com/UFMSPetSistemas/getpet/model/entities/Usuario.java
@@ -1,21 +1,42 @@
 package com.UFMSPetSistemas.getpet.model.entities;
 
-public class Usuario {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
 
+@Entity
+public class Usuario {
+	private @Id @GeneratedValue
+	Long id;
+
+	@NotEmpty
 	private String nomeCompleto;
 
+	@NotNull
 	private Date dataNascimento;
 
+	@NotEmpty
 	private String endereco;
 
+	@NotEmpty
 	private String cidade;
 
+	@NotEmpty
 	private String uf;
 
+	@Email
+	@NotEmpty
 	private String email;
 
+	@NotEmpty
 	private String telefone;
 
+	@NotEmpty
 	private String senha;
 
 }
